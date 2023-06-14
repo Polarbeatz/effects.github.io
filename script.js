846b80077e7cbf0f5348da53d17ab175f284ca57
Polarbeatz <baringcalritche@gmail.com>
2023-06-14 22:43:12 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+let play = document.getElementById("hahaha");
+function playHahaha() {
+    let audio = new Audio("hahaha.mp3");
+    audio.play()
+}
+    play.addEventListener("click",playHahaha);
+
+
+let play2 = document.getElementById("Toinks");
+function playToinks() {
+    let audio2 = new Audio("Toinks.mp3");
+    audio2.play()
+}
+    play2.addEventListener("click",playToinks);
+
+let play3 = document.getElementById("Yahoo");
+function playYahoo() {
+    let audio3 = new Audio("Yahoo.mp3");
+    audio3.play()
+}
+    play3.addEventListener("click",playYahoo);
+
+
+    let play4 = document.getElementById("Applause");
+function playApplause() {
+    let audio4 = new Audio("Applause.mp3");
+    audio4.play()
+}
+    play4.addEventListener("click",playApplause);
+
+    let play5 = document.getElementById("drumRoll");
+function playdrumRoll() {
+    let audio5 = new Audio("drumRoll.mp3");
+    audio5.play()
+}
+    play5.addEventListener("click",playdrumRoll);
+
+    let play6 = document.getElementById("CymbalCrash");
+function playCymbalCrash() {
+    let audio6 = new Audio("CymbalCrash.mp3");
+    audio6.play()
+}
+    play6.addEventListener("click",playCymbalCrash);
+
+    
+    
